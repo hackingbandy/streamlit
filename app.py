d1aa4f5437d155929b1d5cacdd8340cd35a2bc22
@kudryavtsev.andrey <kudryavtsev.andrey@siemens.com>
2025-02-17 08:51:21 +0100

    add markdown

--- app.py
+++ app.py
@@ -4,3 +4,6 @@ import numpy as np
 
 st.title("Zara Dashboard")
 st.sidebar.title("Menu")
+
+st.markdown(" This app is for training purpose to analyze the zara data from kaggle. 🚀")
+st.sidebar.markdown(" Here will be the Navigation")
