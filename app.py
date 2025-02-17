d4f86d7d95727adab1e9cf5929095a858a42fd34
@kudryavtsev.andrey <kudryavtsev.andrey@siemens.com>
2025-02-17 08:58:20 +0100

    load data into streamlit

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st 
-import pandas as pandas
+import pandas as pd
 import numpy as np
 
 st.title("Zara Dashboard")
@@ -7,3 +7,13 @@ st.sidebar.title("Menu")
 
 st.markdown(" This app is for training purpose to analyze the zara data from kaggle. 🚀")
 st.sidebar.markdown(" Here will be the Navigation")
+
+# load data
+data_url = "data/zara.csv"
+
+@st.cache_data(persist=True) #store data on disk to avoid compute every time we deploy
+def load_data():
+    data = pd.read_csv(data_url)
+    return data
+
+data = load_data()
