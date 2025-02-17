af440bcdb6d8d14a950b34a9cfd58d827eb76c44
@kudryavtsev.andrey <kudryavtsev.andrey@siemens.com>
2025-02-17 09:10:18 +0100

    add radio button

--- app.py
+++ app.py
@@ -20,5 +20,5 @@ data = load_data()
 
 st.write(data)
 
-st.sidebar.subheader("Show random data")
-random_name = st.sidebar.radio()
+st.sidebar.subheader("Show random Sectino")
+random_sectino = st.sidebar.radio('Gender', ('MAN', 'WOMAN'))
